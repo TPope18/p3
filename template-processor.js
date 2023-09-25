8f5e449c46ece7551183400642f573885800385e
Joshua McDonald <144265945+jdon776@users.noreply.github.com>
2023-09-25 13:19:57 -0400

    Create template-processor.js

---
+++ template-processor.js
@@ -0,0 +1,16 @@
+class TemplateProcessor {
+    constructor(template) {
+        this.template = template;
+    }
+    fillIn(dictionary){
+        let returnString = this.template;
+        for (const property in dictionary){
+            if (Object.prototype.hasOwnProperty.call(dictionary, property)) {
+                returnString = returnString.replaceAll("{{" + property + "}}",dictionary[property]);
+            }
+        }
+        const regex = /{{.*}}/g;
+        returnString = returnString.replaceAll(regex,"");
+        return returnString;
+    }
+}
